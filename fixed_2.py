2f2ad4a891d3e52fb0a5dd12b618e2d197c611e3
Unique Orduna <uniqueorduna@swuser-129-8-204-153.swlan.csufresno.edu>
2022-05-11 12:02:59 -0700

    finished

---
+++ fixed_2.py
@@ -0,0 +1,21 @@
+"""
+The program is going to calculate the percent of grades that are above average 
+It gather the grades from the file final.txt
+it will add all grades then calculating the average
+it will return the count and multiply the count by 100 then divide by the count of grades
+
+it will then call main to kick start the next function
+it will open the file and append the grades
+print the number of grads which is the count
+next calculate the avergae 
+print the avergae grade 
+print the percentage of grades that are above avergae 
+close the file
+
+call main to make the function work
+
+
+
+
+
+"""
