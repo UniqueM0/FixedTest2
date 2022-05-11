670f2added9daf62129b22a258c2e5b3e81d12fd
Unique Orduna <uniqueorduna@swuser-129-8-204-153.swlan.csufresno.edu>
2022-05-11 12:30:30 -0700

    finish

--- fixed_2.py
+++ fixed_2.py
@@ -14,6 +14,30 @@ close the file
 
 call main to make the function work
 
+"""
+
+
+"""
+define calculate percent above average ()
+set count equals 0 
+use if statement to explain grade is greater than average score
+add the number of count +1
+then return count * 100 divide by the len of grades
+
+
+define main
+open the file final.txt
+set the term grades to a empty list
+because the file will automate the list with the numbers from file
+appen the grades and strip
+print the number of grades to user
+set average to equals 0
+use a for statement grade in grades
+is average + grade
+set average divided equals length of grades
+print average grade
+print percentage of grade (calculate the percent of grades that are above average)
+close file
 
 
 
